01c9bd918877e37347b59d1d352d124f51a22fb3
twhit <whitsontl@gmail.com>
2020-12-15 11:06:41 -0600

    add assignments from 1.2

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,18 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100 # meters
+bounce_degradation = 3.0 / 5.0 # percentage bounce height decreases each time
+num_of_bounces = 0
+
+
+while num_of_bounces < 10:
+    height = (height * bounce_degradation)
+    num_of_bounces += 1
+    if num_of_bounces == 1:
+        print(num_of_bounces, "bounce,", round(height,4), 'meters')
+    else:
+        print(num_of_bounces, "bounces,", round(height,4), 'meters')
+
+
