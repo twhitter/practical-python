ea1cf6856098fa466eca1eaa65fd8c9b85687665
twhit <whitsontl@gmail.com>
2020-12-15 11:27:20 -0600

    half of chap 1_2 assignments

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,18 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+extra_payment = 1000
+
+while principal > 0:
+    if principal < 12:
+        principal = principal * (1 + rate/12) - (payment + extra_payment)
+    else:
+        principal = principal * (1 + rate/12) - (payment + extra_payment)
+    total_paid = total_paid + payment
+
+print('Total paid', round(total_paid,2))
