5097702ef97ae1976d6ff92028098fbcc2227459
twhit <whitsontl@gmail.com>
2020-12-17 16:27:44 -0600

    fixed mortage.py calcs

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -7,12 +7,16 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 extra_payment = 1000
+months = 0
 
 while principal > 0:
-    if principal < 12:
+    if months < 12:
         principal = principal * (1 + rate/12) - (payment + extra_payment)
+        total_paid = total_paid + (payment + extra_payment)
+        months = months + 1
     else:
-        principal = principal * (1 + rate/12) - (payment + extra_payment)
-    total_paid = total_paid + payment
+        principal = principal * (1 + rate/12) - payment
+        total_paid = total_paid + payment
+        months = months + 1
 
-print('Total paid', round(total_paid,2))
+print('Total paid', round(total_paid,2), 'Months:', months)
