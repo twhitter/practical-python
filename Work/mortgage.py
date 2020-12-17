7cb55a140890afb0a61718b117a98121e863be9d
twhit <whitsontl@gmail.com>
2020-12-17 16:36:36 -0600

    add support for variable payments and payment months

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,17 +6,19 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
-extra_payment = 1000
 months = 0
 
+extra_payment = 1000
+extra_payment_start_month = 1
+extra_payment_end_month = 12
+
 while principal > 0:
-    if months < 12:
-        principal = principal * (1 + rate/12) - (payment + extra_payment)
-        total_paid = total_paid + (payment + extra_payment)
-        months = months + 1
-    else:
-        principal = principal * (1 + rate/12) - payment
-        total_paid = total_paid + payment
-        months = months + 1
+    months = months + 1
+    principal = principal * (1 + rate/12) - payment
+    total_paid = total_paid + payment
+
+    if months >= extra_payment_start_month and months <= extra_payment_end_month:
+        principal = principal - extra_payment
+        total_paid = total_paid + extra_payment      
 
 print('Total paid', round(total_paid,2), 'Months:', months)
